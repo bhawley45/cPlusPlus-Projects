19429a1f60dd25c79829fc43f528a1f24302dbd7
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2024-01-29 06:41:53 -0600

    Updated axe_game.cpp
    Added a falling axe object (green rectangle), Cleaned up player movement by moving to a function, limited player movement to the confines of the current window dimensions.

--- Raylib_Projects/First_Project/axe_game.cpp
+++ Raylib_Projects/First_Project/axe_game.cpp
@@ -1,4 +1,18 @@
-#include "raylib.h";
+#include "raylib.h"
+
+void handleMovement(int &x) // reference to x to prevent taking an uneccessary copy
+{
+    // Move circle right
+    if (IsKeyDown(KEY_D) && x < 640)
+    {
+        x += 3.5;
+    }
+    // Move circle left
+    if (IsKeyDown(KEY_A) && x > 0)
+    {
+        x -= 3.5;
+    }
+}
 
 int main()
 {
@@ -7,8 +21,13 @@ int main()
     int height{480};
     InitWindow(width, height, "Test Window");
 
+    // Circle coordinates
     int circleX{320};
     int circleY{240};
+
+    // Axe coordinates
+    int axeX{150};
+    int axeY{150};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -18,20 +37,17 @@ int main()
         // Begining of game
 
         DrawCircle(circleX, circleY, 25, BLUE);
+        DrawRectangle(axeX, axeY, 50, 50, GREEN);
 
-        // Move circle right
-        if (IsKeyDown(KEY_D))
-        {
-            circleX += 3.5;
-        }
+        // Move axe
+        axeY += 10;
 
-        // Move circle left
-        if (IsKeyDown(KEY_A))
-        {
-            circleX -= 3.5;
-        }
+        handleMovement(circleX);
 
         // End of game
         EndDrawing();
     }
+
+    CloseWindow();
+    return 0;
 }
